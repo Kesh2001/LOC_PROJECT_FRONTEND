109d9faec91a3e301cd66f6c22f183c2e63cb920
Zishan Shaikh <macone394@gmail.com>
2025-03-07 00:24:02 -0500

    Save progress before updating Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AutocompleteComponent } from "../shared/components/autocomplete/autocomplete.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { UserFormComponent } from "../shared/user-form/user-form.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AutocompleteComponent, UserFormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'InternshipProject1_LOC_Frontend';
